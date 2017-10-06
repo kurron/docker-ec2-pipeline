7702846ba538eb7cacdac56cf730bb002b3da35c
Ron Kurr <kurr@kurron.org>
2017-10-06 13:39:44 -0400

    Docker chaining to Docker is working!

--- initiate-deployment-pipeline.sh
+++ initiate-deployment-pipeline.sh
@@ -5,11 +5,22 @@ export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-CHANGEME}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-CHANGEME}
 
+# Environment variables needed to map the Docker user to the user's Environment
+USER_ID=${USER_ID:-$(id -u $(whoami))}
+GROUP_ID=${GROUP_ID:-$(id -g $(whoami))}
+HOME_DIR=${HOME_DIR:-$(cut -d: -f6 < <(getent passwd ${USER_ID}))}
+
 # Environment variables required to identify the correct EC2 instances
 PROJECT=${PROJECT:-Weapon-X}
 ENVIRONMENT=${ENVIRONMENT:-development}
 BASTION_TAG=${BASTION_TAG:-Bastion}
 PRIVATE_TAG=${PRIVATE_TAG:-Docker}
+
+# Environment variables needed to contact Hashicorp's Vault
+VAULT_ADDR=${VAULT_ADDR:-http://192.168.254.90:8200}
+ROLE_ID=${ROLE_ID:-CHANGEME}
+SECRET_ID=${SECRET_ID:-CHANGEME}
+VAULT_PATH=${VAULT_PATH:-CHANGEME}
 
 function determineBastionAddress() {
   local STATE_FILTER=Name=instance-state-name,Values=running
@@ -52,40 +63,31 @@ function determineDockerAddresses() {
 }
 
 function runContainer() {
-  local SSH_GROUP_ID=$(cut -d: -f3 < <(getent group ssh))
-  local USER_ID=$(id -u $(whoami))
-  local GROUP_ID=$(id -g $(whoami))
-  local WORK_AREA=/work-area
-  local HOME_DIR=$(cut -d: -f6 < <(getent passwd ${USER_ID}))
-
-  ANSIBLE="ansible-playbook --user ec2-user \
-                            --inventory ${WORKERS} \
-                            --verbose \
-                            playbook.yml"
-
-  echo ${ANSIBLE}
-
   local CMD="docker run --net host \
                   --add-host bastion:${BASTION} \
                   --hostname inside-docker \
                   --env HOME=${HOME_DIR} \
-                  --env SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+                  --env ANSIBLE_CONFIG=/tmp/ansible.cfg \
+                  --env VAULT_ADDR=${VAULT_ADDR} \
+                  --env ROLE_ID=${ROLE_ID} \
+                  --env SECRET_ID=${SECRET_ID} \
+                  --env VAULT_PATH=${VAULT_PATH} \
+                  --env WORKERS=${WORKERS} \
                   --interactive \
                   --name deployer-test \
                   --rm \
                   --tty \
                   --user=${USER_ID}:${GROUP_ID} \
-                  --volume ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} \
-                  --volume $(pwd):$(pwd) \
                   --volume ${HOME_DIR}:${HOME_DIR} \
+                  --volume ${PLAYBOOK}:${HOME_DIR}/playbook.yml \
                   --volume /etc/passwd:/etc/passwd \
                   --volume /etc/group:/etc/group \
-                  --workdir $(pwd) \
-                  dockeransible2bastionaccess_deployer:latest ${ANSIBLE}"
+                  --workdir ${HOME_DIR} \
+                  kurron/docker-ansible:latest /tmp/deploy-docker-containers.sh"
   echo $CMD
   $CMD
 }
 
-determineBastionAddress ${REGION} ${PROJECT} ${ENVIRONMENT} Bastion
-determineDockerAddresses ${REGION} ${PROJECT} ${ENVIRONMENT} Docker
-#runContainer
+determineBastionAddress
+determineDockerAddresses
+runContainer
--- test.sh
+++ test.sh
@@ -5,6 +5,18 @@ AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-CHANGEME}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-CHANGEME}
 
+# Environment variables needed to contact Hashicorp's Vault
+VAULT_ADDR=${VAULT_ADDR:-http://192.168.254.90:8200}
+ROLE_ID=${ROLE_ID:-CHANGEME}
+SECRET_ID=${SECRET_ID:-CHANGEME}
+VAULT_PATH=${VAULT_PATH:-CHANGEME}
+
+# We need to be part of the docker group to interact with the Docker daemon
+DOCKER_GROUP_ID=$(cut -d: -f3 < <(getent group docker))
+
+# Environment variable describing the location of the playbook to run
+PLAYBOOK=${PLAYBOOK:-$(pwd)/playbook.yml}
+
 function runContainer() {
   local USER_ID=$(id -u $(whoami))
   local GROUP_ID=$(id -g $(whoami))
@@ -12,11 +24,17 @@ function runContainer() {
   local HOME_DIR=$(cut -d: -f6 < <(getent passwd ${USER_ID}))
 
   local CMD="docker run --net host \
+                  --group-add ${DOCKER_GROUP_ID} \
                   --hostname inside-docker \
                   --env HOME=${HOME_DIR} \
                   --env AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} \
                   --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
                   --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+                  --env VAULT_ADDR=${VAULT_ADDR} \
+                  --env ROLE_ID=${ROLE_ID} \
+                  --env SECRET_ID=${SECRET_ID} \
+                  --env VAULT_PATH=${VAULT_PATH} \
+                  --env PLAYBOOK=${PLAYBOOK} \
                   --interactive \
                   --name ec2-docker-pipeline \
                   --rm \
@@ -26,8 +44,9 @@ function runContainer() {
                   --volume ${HOME_DIR}:${HOME_DIR} \
                   --volume /etc/passwd:/etc/passwd \
                   --volume /etc/group:/etc/group \
+                  --volume /var/run/docker.sock:/var/run/docker.sock \
                   --workdir $(pwd) \
-                  dockerec2tagsearch_deployer:latest bash"
+                  dockerec2tagsearch_deployer:latest ./initiate-deployment-pipeline.sh"
   echo $CMD
   $CMD
 }
